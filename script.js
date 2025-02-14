37227b049500ac245f4eb071921d0345d92f90d7
talittaamorim <talitasilva.web@gmail.com>
2025-02-14 16:15:13 -0300

    start

---
+++ script.js
@@ -0,0 +1,13 @@
+const chave = "791b131695d63f60723a66849b8f8d74"
+
+async function buscarCidade(cidade){
+    
+}
+
+function pegarDados(){
+    let cidade = document.querySelector("enviar-dados").vaLue
+
+    buscarCidade(cidade)
+}
+
+
